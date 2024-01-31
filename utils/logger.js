5c487b37d808047dadba849605ec08ac9b33206f
evhenious <evhenious@gmail.com>
2024-01-31 19:08:11 +0200

    simplified log levels

--- utils/logger.js
+++ utils/logger.js
@@ -8,10 +8,22 @@ function logger(value) {
     colors.disable(); // it is ON by default, so we switch it OFF when need
   }
 
+  const loggerWeight = {
+    info: 1,
+    warn: 2,
+    error: 3
+  }
+
   return {
-    info: (...arr) => console.log(colors.bgGreen(`${value}:`), ...arr),
-    warn: (...arr) => console.warn(colors.bgYellow(`${value}:`), ...arr),
-    error: (...arr) => console.error(colors.bgRed(`${value}:`), ...arr)
+    info: (...arr) => {
+      if (loggerWeight[logLevel] !== loggerWeight.info) return; //! info works ONLY when 'info' level set
+      console.log(colors.bgGreen(`${value}:`), ...arr)
+    },
+    warn: (...arr) => {
+      if (loggerWeight[logLevel] > loggerWeight.warn) return; //! warn works when 'info' or 'warn' level set
+      console.warn(colors.bgYellow(`${value}:`), ...arr)
+    },
+    error: (...arr) => console.error(colors.bgRed(`${value}:`), ...arr) //! error level works ALWAYS :)
   };
 }
 
