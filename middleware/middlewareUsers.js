51a61026d4c9dfe5fd3e3d83bf782dabcdf31239
VladislavkiSelov <vladislavkiselev.2007@gmail.com>
2024-03-02 12:43:07 +0200

    add database + update routerUser

--- middleware/middlewareUsers.js
+++ middleware/middlewareUsers.js
@@ -1,4 +1,7 @@
 const yup = require("yup");
+const knexLib = require("knex");
+const knexConfig = require("../knexfile");
+const knex = knexLib(knexConfig);
 
 async function validateUserData(req, res, next) {
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/;
@@ -9,6 +12,16 @@ async function validateUserData(req, res, next) {
 
   try {
     await userSchema.validate(req.body);
+    next();
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function createUser(req, res, next) {
+  try {
+    const [newUser] = await knex("users").insert(req.body).returning("*");
+    req.newUser = newUser;
     next();
   } catch (err) {
     next(err);
@@ -28,6 +41,21 @@ async function validateUserId(req, res, next) {
   }
 }
 
+async function seachUserId(req, res, next) {
+  const user = await knex.select().from("users").where({ id: req.params.userId });
+  try {
+    if (user.length === 0) {
+      req.status = 404;
+      throw new Error(`User with ID "${req.params.userId}" not found`);
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
+}
+
 async function validateUserDelete(req, res, next) {
   const userSchema = yup.object({
     userId: yup.number().positive().min(0),
@@ -41,13 +69,31 @@ async function validateUserDelete(req, res, next) {
   }
 }
 
-function routerUserError(err, req, res, next) {
-    res.status(req.status || 400).send({ error: err.message });
+async function userDelete(req, res, next) {
+  const result = await knex.delete().from("users").where({ id: req.params.userId });
+
+  try {
+    if (!result) {
+      req.status = 404;
+      throw new Error(`User with ID "${req.params.userId}" not found`);
+    }
+
+    next();
+  } catch (err) {
+    next(err);
   }
+}
+
+function routerUserError(err, req, res, next) {
+  res.status(req.status || 400).send({ error: err.message });
+}
 
 module.exports = {
   validateUserData,
+  createUser,
   validateUserId,
+  seachUserId,
   validateUserDelete,
-  routerUserError
+  userDelete,
+  routerUserError,
 };
--- routes/routerUser.js
+++ routes/routerUser.js
@@ -1,63 +1,43 @@
 const router = require("express").Router();
-const fs = require("fs");
-const path = require("path");
-const { validateUserData, validateUserId, validateUserDelete, routerUserError } = require("../middleware/middlewareUsers");
 
-/** @type { { data: { username: string, email: string, id: numer }[] } } */
-let userDb = JSON.parse(fs.readFileSync(path.join(".", "usersJson.json"), "utf8"));
+const {
+  validateUserData,
+  createUser,
+  validateUserId,
+  seachUserId,
+  validateUserDelete,
+  userDelete,
+  routerUserError,
+} = require("../middleware/middlewareUsers");
 
-function createUser(req, res) {
-  const newUser = {
-    ...req.body,
-    id: Date.now()
-  };
+const knexLib = require("knex");
+const knexConfig = require("../knexfile");
+const knex = knexLib(knexConfig);
 
-  userDb.data.push(newUser);
-
-  res.send(201, newUser); //! повертаємо дані збереженого юзера, разом з його айді
+async function giveCreateUser(req, res) {
+  res.send(201, req.newUser);
 }
 
-function giveUserId(req, res) {
-  const user = userDb.data.find((user) => user.id === +req.params.userId);
-
-  if (!user) {
-    req.status = 404;
-    throw new Error(`User with ID "${req.params.userId}" not found`);
-  }
-
-  res.send(200, user);
+async function giveUserId(req, res) {
+  res.send(200, req.user);
 }
 
-function userDelete(req, res) {
-  const userId = userDb.data.findIndex((user) => user.id === +req.params.userId)
-
-  if (userId >= 0) {
-    userDb.data.splice(userId, 1);
-    res.send(204);
-  } else {
-    req.status = 404;
-    throw new Error(`User with ID "${userId}" not found`);
-  }
-
+async function giveStatusUserDelete(req, res) {
+  res.send(204);
 }
 
-router.get("/", (req, res) => {
-  // давай будемо однотипними - якщо на getUserById повертаємо просто об'єкт юзера без враппера { data: ...},
-  // то давай і тут так само
-  res.send(200, userDb.data);
+router.get("/", async (req, res) => {
+  const allUsers = await knex.select().from("users");
+  res.send(200, allUsers);
 });
 
-router.post("/", validateUserData, createUser);
+router.post("/", validateUserData, createUser, giveCreateUser);
 
-router.get("/:userId", validateUserId, giveUserId);
+router.get("/:userId", validateUserId, seachUserId, giveUserId);
 
-router.delete("/:userId", validateUserDelete, userDelete);
+router.delete("/:userId", validateUserDelete, userDelete, giveStatusUserDelete);
 
 router.use(routerUserError);
-
-process.on("SIGINT", () => {
-  fs.writeFileSync(path.join(".", "usersJson.json"), JSON.stringify(userDb));
-});
 
 module.exports = {
   router,
